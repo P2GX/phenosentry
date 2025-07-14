c6d8d98eb36074c0c57591b00f7aa308848de47c
iimpulse <Michael.Gargano@jax.org>
2025-07-14 09:30:01 -0600

    reader private, update optional deps to cli

---
+++ phenosentry/__init__.py
@@ -0,0 +1,8 @@
+"""
+Phenopacket Store Toolkit helps with Phenopacket Store release and Q/C
+and simplifies access to the store data for the downstream applications.
+"""
+from . import validation
+__all__ = [
+    "validation"
+]
