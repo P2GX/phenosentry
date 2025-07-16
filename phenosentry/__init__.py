5c44aa2377651dd206df3d56371fabf1b4bc6ff4
iimpulse <Michael.Gargano@jax.org>
2025-07-16 08:44:16 -0600

    remove errenous ai comment

--- phenosentry/__init__.py
+++ phenosentry/__init__.py
@@ -1,7 +1,3 @@
-"""
-Phenopacket Store Toolkit helps with Phenopacket Store release and Q/C
-and simplifies access to the store data for the downstream applications.
-"""
 from . import validation
 __all__ = [
     "validation"
